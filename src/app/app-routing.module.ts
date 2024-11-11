96698a9e87415ad37df9ec5888f7dd9bbfc58455
massawegodii <massawegodfrey43@gmail.com>
2024-11-12 00:04:11 +0300

    landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: WelcomeComponent }
+  { path: '', component: WelcomeComponent },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const savedCategory = localStorage.getItem('selectedCategory');
+      if (savedCategory) {
+        this.selectedCategory = savedCategory;
+      }
+    }
+  }
+
+  isLoginModalOpen = false;
+  isSignupModalOpen = false;
+  selectedCategory: string = 'all';
+
+  openModal(type: string) {
+    if (type === 'login') {
+      this.isLoginModalOpen = true;
+    } else if (type === 'signup') {
+      this.isSignupModalOpen = true;
+    }
+    setTimeout(() => {
+      const modal = document.querySelector('.modal-animation');
+      if (modal) modal.classList.add('scale-100');
+    }, 10);
+  }
+
+  closeModal() {
+    const modal = document.querySelector('.modal-animation');
+    if (modal) modal.classList.remove('scale-100');
+    setTimeout(() => {
+      this.isLoginModalOpen = false;
+      this.isSignupModalOpen = false;
+    }, 300); 
+  }
+
+  selectCategory(category: string): void {
+    this.selectedCategory = category;
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('selectedCategory', category);
+    }
+  }
+
 
 }
